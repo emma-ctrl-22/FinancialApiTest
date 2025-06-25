20ed73a07fb605c92a905dd6468bfb454ff9024a
emma-ctrl-22 <enyatepe.ac@st.vvu.edu.gh>
2025-06-25 20:35:23 +0000

    project Setup

---
+++ src/test/java/com/example/financialapiapplication/FinancialApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.financialapiapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancialApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
